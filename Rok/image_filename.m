92520449c120ff5c6fabdf034849ab1734abfafc
soline-chanet <soline.chanet@gmail.com>
2014-05-12 16:48:37 -0400

    Initial_Commit

---
+++ Rok/image_filename.m
@@ -0,0 +1,14 @@
+function data = image_filename(time_i, layer_i, src)
+% ** This is an automatically generated function
+% ** created at 5/9/2014, 6:46 PM by write_image_filename_function.m
+% ** Inputs the time, layer, and source directory of a data set.
+% ** Outputs the filename of that image.
+
+% ** For data set: Spn5-Rok-aPKC-Myo
+
+filename = 'Rok_z001.tif';
+
+z_name = sprintf(strcat('%.', num2str(3), 'u'), layer_i);
+filename(6:6+3-1) = z_name;
+
+data = fullfile(src, filename);
